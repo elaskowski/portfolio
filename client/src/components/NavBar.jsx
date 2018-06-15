2a80ff6ab81da83b192986c260129a96bd836fed
elaskowski <evalaskows@gmail.com>
2018-06-14 18:52:51 -0700

    Align navbar content right and update navbar links

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -10,9 +10,9 @@ class NavBar extends React.Component {
           <li className="title">EVA LASKOWSKI</li>
           <li className="job">WEB DEVELOPER</li>
           <li><a href="#home">Home</a></li>
-          <li><a href="#news">Portfolio</a></li>
-          <li><a href="#contact">About</a></li>
-          <li><a href="#about">Contact</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
       </div>
     );
