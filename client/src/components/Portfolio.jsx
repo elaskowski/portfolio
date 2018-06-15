216a39acc3dad437dab514bd4d96a87fcdd0eaa2
elaskowski <evalaskows@gmail.com>
2018-06-14 18:48:01 -0700

    Update Portfolio CSS with new font size

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
 import Project from './Project.jsx';
+import ProjectNR from './ProjectNR.jsx';
 
 import './styles/Portfolio.css';
 
 class Portfolio extends React.Component {
   render() {
     return (
-      <div className="portfolio">
+      <div className="portfolio" data-aos="fade-up">
         <h2>PORTFOLIO</h2>
         <Project />
+        <ProjectNR />
+        <ProjectNR />
+        <ProjectNR />
       </div>
     );
   }
