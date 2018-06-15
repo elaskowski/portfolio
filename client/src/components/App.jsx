4bb356dbb818bb6b9dfc9177adc553d411c3506e
elaskowski <evalaskows@gmail.com>
2018-06-14 18:47:05 -0700

    Implemented scrolling funtionality for project page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import BrowserRouter from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 import NavBar from './NavBar.jsx';
 import Portfolio from './Portfolio.jsx';
@@ -7,6 +9,9 @@ import Portfolio from './Portfolio.jsx';
 import './styles/App.css';
 
 class App extends React.Component {
+  componentDidMount() {
+    AOS.init({ duration: 1000 });
+  }
   render() {
     return (
       <div className="row">
