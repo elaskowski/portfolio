7752c4998737dfe0fffc62a03d1a436793209cf3
elaskowski <evalaskows@gmail.com>
2018-06-25 12:52:49 -0700

    Updated typos

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -12,7 +12,7 @@ class About extends React.Component {
       <div className="portfolio" data-aos="fade-up">
         <h2>ABOUT ME</h2>
         <div className="rectangle">
-          <div className="bio">Hello! <br /><br />I was drawn to software engineering because it weaves together creativity and logic. Each challenge can be solved using a unique approach.<br /><br />I have been a lead developer on multiple applications using agile development, developing feature-rich user experiences by architecting RESTful NodeJS APIs that communicate with relational and non-relational databases. Throughout my work, I strive to create engaging and intuitive UX.<br /> <br />In my spare time, I enjoy reading, spending time outside, trying (and usually failing) to cook new dishes, or catching up on Game on Thrones, Westworld, Stranger Things, or any true crime podcast.</div>
+          <div className="bio">Hello! <br /><br />I was drawn to software engineering because it weaves together creativity and logic. Each challenge can be solved using a unique approach.<br /><br />I have been a lead developer on multiple applications using agile development, developing feature-rich user experiences by architecting RESTful NodeJS APIs that communicate with relational and non-relational databases. Throughout my work, I strive to create engaging and intuitive UX.<br /> <br />In my spare time, I enjoy reading, spending time outside, trying (and usually failing) to cook new dishes, or catching up on Game of Thrones, Westworld, Stranger Things, or any true crime podcast.</div>
           <div className="headshot"><img className="image" src="https://i.imgur.com/BXeXWid.jpg" /></div>
         </div>
       </div>
--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => (
   <div className="contact" data-aos="fade-up">
     <h2>CONTACT ME</h2>
     <form method="POST" action="https://formspree.io/evalaskows@gmail.com">
-      <input type="email" name="email" placeholder="Email" /><br /><br />
+      <input type="email" name="email" placeholder="Your Email" /><br /><br />
       <textarea name="message" placeholder="Message" /><br /><br />
       <button type="submit">SEND</button>
     </form>
