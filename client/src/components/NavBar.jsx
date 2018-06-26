61bcdc350cbf0b7a0bd828c8e9637fdb3e45d726
elaskowski <evalaskows@gmail.com>
2018-06-25 17:17:50 -0700

    Added logo to navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,21 +6,28 @@ class NavBar extends React.Component {
   render() {
     return (
       <div className="navbar">
-        <ul className="navbar-list">
-          {/* <li className="title">EVA LASKOWSKI</li>
-          <li className="job">WEB DEVELOPER</li> */}
-          <li className="link"><a href="#home">Home</a></li>
-          <li className="link"><a href="#about">About Me</a></li>
-          <li className="link"><a href="#portfolio">Portfolio</a></li>
-          <li className="link"><a href="#skills">Skills</a></li>
-          <li className="link"><a href="#contact">Contact</a></li>
-        </ul>
+        <div className="nav-container">
+          <img
+            src="https://i.imgur.com/5Q5GFEY.png"
+            style={{
+              height: '100px',
+              width: '100px',
+              'padding-left': '59px',
+              'padding-top': '20px',
+            }}
+            alt="Eva Logo"
+          />
+          <ul className="navbar-list">
+            <li className="link"><a href="#home">Home</a></li>
+            <li className="link"><a href="#about">About Me</a></li>
+            <li className="link"><a href="#portfolio">Portfolio</a></li>
+            <li className="link"><a href="#skills">Skills</a></li>
+            <li className="link"><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
       </div>
     );
   }
 }
 
-// active??
-
-// <li><a className="active" href="#home">Home</a></li>
 export default NavBar;
